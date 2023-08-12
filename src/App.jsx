d8685c26325711487e4fadf3f1c6fc0c486d2c44
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-12 16:47:23 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={"title"}>
+        The Food Club
+      </h1>
+    </div>
+  )
+}
+
+export default App
