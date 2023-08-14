1f81e5bef90db7348e9b9780c685a374bdd3f1a8
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-14 18:50:04 +0200

    update vite config to allow connection with backend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr';
+import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: 'REACT_APP_',
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  plugins: [
+      react(),
+      svgrPlugin({
+        svgrOptions: {
+          icon: true,
+        },
+      }),
+  ]
 })
