a037769c2e72c173f266b2956fe090ca1c99fc0f
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-15 15:50:46 +0200

    init list

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Home = () => {
+    return(
+        <div className="container BG_pink">
+            <h1 className={"title FC"}>
+                The Food Club
+            </h1>
+            <p style={{fontStyle:"oblique"}}>
+                A soon to be By-Product of a Lifestyle Obsession.
+            </p>
+        </div>
+    )
+}
+
+export default Home
+
+
