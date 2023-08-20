7a000d9e24e81359614dfb558cb934ec4cbce9de
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-20 14:43:45 +0200

    update map

--- src/elements/map.jsx
+++ src/elements/map.jsx
@@ -9,7 +9,8 @@ const map = () => {
             <MapContainer center={[51.0544, 3.7256]} zoom={13}>
                 <TileLayer
                     attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-                    url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    //url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://api.mapbox.com/styles/v1/oliviervd-tfc/clljebsk0018v01plaeyy7xny/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xpdmllcnZkLXRmYyIsImEiOiJjbGxqZWFjd3MweTBzM2psaWFiemlnZnZnIn0.fMu0iJpz82mNYQ5Rrrwi-w"
                 />
             </MapContainer>
         </div>
