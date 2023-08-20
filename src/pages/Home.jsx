bf5ab29886ed658ff3bec0abae3ebe2cce39e50c
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-20 19:24:57 +0200

    update ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,16 @@ import React from "react"
 
 const Home = () => {
     return(
-        <div className={"container"}>
+        <div className={"container"} style={{background: "white", height: "70vh", paddingTop: "30vh", overflow:"hidden"}}>
             <h1 className={"title FC"}>
-                 The Food Club
+                 The
+            </h1><h1 className={"title FC"}>
+                 FOOD
+            </h1><h1 className={"title FC"}>
+                 Club
             </h1>
-            <p style={{fontStyle:"oblique"}}>
+            <p style={{fontStyle:"title", position: "absolute", top:"55vh", fontSize: "2em", textAlign:"center", width:"100vw"}}>
+                enter
             </p>
         </div>
     )
