7e3127ed43bff86aa4a57336b4dee61f5019d49a
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-20 13:38:03 +0200

    update map

--- src/elements/map.jsx
+++ src/elements/map.jsx
@@ -3,12 +3,15 @@ import {Marker, Popup, TileLayer, MapContainer} from "react-leaflet";
 import React from "react";
 
 const map = () => {
+
     return(
         <div className={"container"}>
-            <MapContainer center={[51.0544, 3.7256]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <MapContainer center={[51.0544, 3.7256]} zoom={13}>
+                <TileLayer
+                    attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
             </MapContainer>
-
         </div>
     )
 }
