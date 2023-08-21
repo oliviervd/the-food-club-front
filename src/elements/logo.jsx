c7caab5825cfcdb26b059a1013d14d26fea58b2e
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-21 20:30:54 +0200

    init logo

---
+++ src/elements/logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import _im from "./Untitled_Artwork.png"
+
+const Logo = () => {
+    return(
+        <div>
+           <img className={"header--logo"} src={_im}/>
+        </div>
+    )
+}
+
+export default Logo;
