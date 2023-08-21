2c0a2e4ca5dcbca12f422752ab9838e9a7de1a98
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-21 21:06:53 +0200

    add gridUI

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import navbar from "../elements/navbar.jsx";
 import Header from "../elements/header.jsx";
 
+import GridUI from "../elements/Grid.png"
+
 const Categories = () => {
     return(
         <div className={"container"}>
             <Header slug={"Eat like locals"} title={"Food Club"}></Header>
+            <img className={"UI-GRID"} src={GridUI}/>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {useNavigate} from "react-router-dom";
 import _im from "/src/elements/Untitled_Artwork.png"
+import GridUI from "../elements/Grid.png"
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
 
     return(
         <div className={"container home"}>
+            <img className={"UI-GRID"} src={GridUI}/>
             <div className={"vertical-center"}>
                 <img className={"home--logo"} src={_im}/>
             </div>
