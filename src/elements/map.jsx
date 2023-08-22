ff953f90085180619c3fc950889895de8848b488
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-22 18:54:20 +0200

    rework UI mapBox

--- src/elements/map.jsx
+++ src/elements/map.jsx
@@ -24,7 +24,7 @@ const map = () => {
                 <TileLayer
                     attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
                     //url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    url="https://api.mapbox.com/styles/v1/oliviervd-tfc/clljebsk0018v01plaeyy7xny/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xpdmllcnZkLXRmYyIsImEiOiJjbGxqZWFjd3MweTBzM2psaWFiemlnZnZnIn0.fMu0iJpz82mNYQ5Rrrwi-w"
+                    url="https://api.mapbox.com/styles/v1/oliviervd-tfc/clllwhqvq009s01pea2rw8mpt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xpdmllcnZkLXRmYyIsImEiOiJjbGxqZWFjd3MweTBzM2psaWFiemlnZnZnIn0.fMu0iJpz82mNYQ5Rrrwi-w"
                 />
 
                 {MarkerLocations.map(marker => (
