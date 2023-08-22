d34451af56a22985afd2ad5cdc3785a660e7de8a
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-22 18:53:38 +0200

    init hamburger menu button

--- src/elements/header.jsx
+++ src/elements/header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "./logo.jsx";
+import NavBar from "./navbar.jsx";
 
 const Header = (props) => {
     return(
         <div className={"header--container"}>
             <Logo></Logo>
             <p className={"header--slug"}>{props.slug}</p>
+            <NavBar/>
         </div>
     )
 }
--- src/elements/navbar.jsx
+++ src/elements/navbar.jsx
@@ -1,19 +1,12 @@
 import React from "react";
+import _hamburger from "./images/Hamburger.png"
+
 
 const NavBar = (props) => {
 
     return(
         <div className={"sticky-nav"}>
-            <div className={"sticky-nav__item"}>
-                <div className={"sticky-nav__button"} onClick={()=>props.openMenu(!props.isOpenMenu)}>
-                    <svg viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect id="menu-icon__b" x="0.763672" y="15.8491" width="21.8893" height="3.76579" rx="1.88289" fill="#eee3e2"></rect>
-                        <rect id="menu-icon__m" x="0.763672" y="8.03137" width="21.8893" height="3.76579" rx="1.88289" fill="#eee3e2"></rect>
-                        <rect id="menu-icon__t" x="0.763672" y="0.213867" width="21.8893" height="3.76579" rx="1.88289" fill="#eee3e2"></rect>
-                    </svg>
-                </div>
-            </div>
-
+            <img className={"sticky-nav__hamburger"} src={_hamburger}/>
         </div>
     )
 }
