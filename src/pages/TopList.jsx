4e1849bd48189bf1b4519e321fb412dec73ea313
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-22 19:28:38 +0200

    init UI toplist

--- src/pages/TopList.jsx
+++ src/pages/TopList.jsx
@@ -2,11 +2,19 @@ import React, {useState} from "react";
 import Header from "../elements/header.jsx";
 import NavBar from "../elements/navbar.jsx"
 import NavMenu from "../elements/navMenu.jsx";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {fetchAPI} from "../utils/utils.jsx";
+import GridUI from "../elements/images/Grid.png";
 
 const TopList = () => {
 
     const [openNavMenu, setOpenNavMenu] = useState(false);
+
+    let _categories;
+    let _cats = [];
+
+    const nav = useNavigate()
+
 
     if(openNavMenu){
         document.body.style.overflow = 'hidden';
@@ -26,6 +34,24 @@ const TopList = () => {
         <div className={"container BG_pink"}>
             <Header pageTitle={""}/>
             <NavBar openMenu={setOpenNavMenu} isOpenMenu={openNavMenu}/>
+            <br/>
+
+            <img className={"UI-GRID"} src={GridUI}/>
+
+            <div className={"categories--container"}>
+                <div>
+                    <h2 className={"toplist--title_font"}>
+                        CHOLESTEROL-LOL
+                    </h2>
+                    <p></p>
+                </div>
+                <div className={"toplist--image_container"}>
+                    <div className={"toplist--image"}>
+                    </div>
+                </div>
+
+                <br/>
+            </div>
         </div>
 
     )
