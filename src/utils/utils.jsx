de7dc1dd944d1ade1918806c139be82321c8cfbe
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-24 14:39:31 +0200

    Update utils.jsx

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -4,7 +4,7 @@ export function fetchAPI(endpoint) {
     const {data, isLoading, status} = useQuery({
         queryKey:[endpoint],
         queryFn: () =>
-            fetch(`https://p01--cms--2y8cq25kbv7n.code.run/api/${endpoint}/`,{
+            fetch(`https://p01--cms--j4bvc8vdjtjb.code.run/api/${endpoint}/`,{
                 credentials: 'include',
                 method: 'GET'
             }).then((req)=>req.json())
