eb82517f58c440317a5f79da4ad1a24574a80b3c
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-24 19:21:06 +0200

    onLoad scroll to top/

--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useParams} from "react-router-dom";
 import Header from "../elements/header.jsx";
 import NavBar from "../elements/navbar.jsx";
@@ -6,9 +6,13 @@ import GridUI from "../elements/images/Grid_Dense.png";
 
 const Venue = () => {
 
+    // fetch content based on id
     let id = useParams(); // use id to set content
 
-    // fetch content based on id
+    // onLoad scroll to top.
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
     return(
         <div className={"container BG_pink"}>
