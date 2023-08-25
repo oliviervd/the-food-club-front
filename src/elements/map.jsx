e1a20f2090c7fed5163fb470e740ebd75ead020b
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-25 07:33:04 +0200

    update map - init popup - parse geo from api

--- src/elements/map.jsx
+++ src/elements/map.jsx
@@ -1,20 +1,25 @@
 import {Icon} from "leaflet/dist/leaflet-src.esm.js";
 import {Marker, Popup, TileLayer, MapContainer} from "react-leaflet";
-import React from "react";
+import React, {useState} from "react";
 import _im from "./images/Mapicon.png"
 import GridUI from "./images/Grid_Dense.png";
+import {fetchAPI} from "../utils/utils.jsx";
 
 
 const map = () => {
 
-    const MarkerLocations = [
-        {
-            geoCode: [51.05904316431157, 3.7270031691456937]
-        },
-        {
-            geoCode: [51.05361111787999, 3.723295053804015]
-        }
-    ]
+    const [openInfoPane, setOpenInfoPane] = useState(true)
+
+    console.log(openInfoPane)
+
+    // fetch data
+    let _venues;
+    try{
+        let _venueList = fetchAPI('venue')
+        _venues = _venueList["docs"]
+        console.log(_venueList["docs"])
+    } catch(e) {console.log(e)}
+
 
     const CustomIcon = new Icon({
         iconUrl: _im,
@@ -23,7 +28,7 @@ const map = () => {
 
     return(
         <div className={"container"}>
-            <img className={"UI-GRID"} src={GridUI}/>
+            <img className={"UI-GRID"} src={GridUI} style={{height:"100vh", objectFit:"cover"}}/>
             <MapContainer className={"map--ui"} center={[51.0544, 3.7256]} zoom={14}>
                 <TileLayer
                     attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
@@ -31,14 +36,28 @@ const map = () => {
                     url="https://api.mapbox.com/styles/v1/oliviervd-tfc/clllwhqvq009s01pea2rw8mpt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xpdmllcnZkLXRmYyIsImEiOiJjbGxqZWFjd3MweTBzM2psaWFiemlnZnZnIn0.fMu0iJpz82mNYQ5Rrrwi-w"
                 />
 
-                {MarkerLocations.map(marker => (
-                    <Marker
-                        position={marker.geoCode}
-                        icon={CustomIcon}
-                    />
-                ))}
+                {_venues &&
+                    <div>
+                        {_venues.map(venue => (
+                            <Marker
+                                position={[venue.address.longitude, venue.address.latitude]} icon={CustomIcon}
+                                eventHandlers={{click:(e)=>{
+                                    setOpenInfoPane(true)
+                                    },
+                                }}
+                            >
+                            </Marker>
+                        ))}
+                    </div>
+
+                }
 
             </MapContainer>
+            {openInfoPane &&
+                <div className={"map--ui_pop-up-container"}>
+                    <img className={"UI-GRID__popup"}  src={GridUI}></img>
+                </div>
+            }
         </div>
     )
 }
