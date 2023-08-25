3c6fc5138f85cd8bb1aa739a8421334f7c6b8399
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-25 09:09:18 +0200

    add button to map

--- src/elements/header.jsx
+++ src/elements/header.jsx
@@ -6,7 +6,10 @@ const Header = (props) => {
     return(
         <div className={"header--container"}>
             <Logo></Logo>
-            <p className={"header--slug"}>{props.slug}</p>
+            <div>
+                <h2 className={"header--button_map"}>map</h2>
+                <p className={"header--slug"}>{props.slug}</p>
+            </div>
         </div>
     )
 }
