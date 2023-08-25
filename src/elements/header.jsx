75770406e4ea948d27a55bad2285405fa63f7023
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-25 09:10:44 +0200

    add button to map

--- src/elements/header.jsx
+++ src/elements/header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Logo from "./logo.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Header = (props) => {
+
+    const nav = useNavigate()
 
     return(
         <div className={"header--container"}>
             <Logo></Logo>
             <div>
-                <h2 className={"header--button_map"}>map</h2>
+                <h2 onClick={()=>nav("/")} className={"header--button_map"}>map</h2>
                 <p className={"header--slug"}>{props.slug}</p>
             </div>
         </div>
