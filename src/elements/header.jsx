23922de109c5485c04557b80ffd0d61c19c38fd1
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-25 12:56:09 +0200

    add buttons to header

--- src/elements/header.jsx
+++ src/elements/header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "./logo.jsx";
 import {useNavigate} from "react-router-dom";
+import _map from "./images/map.png"
+import _list from "./images/list.png"
 
 const Header = (props) => {
 
@@ -10,7 +12,14 @@ const Header = (props) => {
         <div className={"header--container"}>
             <Logo></Logo>
             <div>
-                <h2 onClick={()=>nav("/")} className={"header--button_map"}>map</h2>
+                <div style={{display: "flex", gap: "3px"}}>
+                    <div className={"header--button_container"}>
+                        <img onClick={()=>nav("/")} className={"header--button"} src={_map}></img>
+                    </div>
+                    <div className={"header--button_container"}>
+                        <img onClick={()=>nav("/categories")} className={"header--button"} src={_list}></img>
+                    </div>
+                </div>
                 <p className={"header--slug"}>{props.slug}</p>
             </div>
         </div>
