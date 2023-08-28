772259bb675d58f6113c01908c31b0232af603b4
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-28 17:09:12 +0200

    restructure UI (B)

---
+++ src/elements/header_B.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header_B = () => {
+    return(
+        <div className={"headerB--container"}>
+            <h1 className={"headerB--title_typo"}></h1>
+        </div>
+    )
+}
+
+export default Header_B
