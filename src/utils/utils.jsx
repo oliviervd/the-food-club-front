3ca405fec642915575cf636d18d9356330840a09
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-30 17:54:32 +0200

    update limit to make sure all data is collected from REST Api

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,10 +1,11 @@
 import {useQuery} from "@tanstack/react-query";
 
 export function fetchAPI(endpoint) {
+    // fetch all data from a certain collection.
     const {data, isLoading, status} = useQuery({
         queryKey:[endpoint],
         queryFn: () =>
-            fetch(`https://p01--cms--j4bvc8vdjtjb.code.run/api/${endpoint}/`,{
+            fetch(`https://p01--cms--j4bvc8vdjtjb.code.run/api/${endpoint}/?limit=0`,{
                 credentials: 'include',
                 method: 'GET'
             }).then((req)=>req.json())
