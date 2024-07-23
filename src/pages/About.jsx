23eaeb06f75da9c5d63af4a4d3947057d9dc5906
oliviervd <oliviervandhuynslager@gmail.com>
2024-07-23 13:01:57 +0200

    add init about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,15 @@ import Header from "../elements/Header.jsx";
 const About = ({}) => {
     return(
         <div>
-            <Header landing={true} />
-            <p>about comes here</p>
+            <Header landing={true}/>
+            <div className={"about"}>
+                <h1>RULE #1.</h1>
+                <p>ipsem lorum la autro quisto</p>
+                <h1>RULE #2.</h1>
+                <p>ipsem lorum la autro quisto</p>
+                <h1>RULE #3.</h1>
+                <p>ipsem lorum la autro quisto</p>
+            </div>
         </div>
     )
 }
