c70eaf0d008bb85201459ae1a649b88ac0da49e7
oliviervd <oliviervandhuynslager@gmail.com>
2024-08-03 17:12:41 +0200

    Commit message:
    Add category fetching and display logic in Categories.jsx
    
    Implemented `useEffect` to fetch category data and updated state with the results. Updated CSS to style the category container and ensure proper layout and appearance. Removed unnecessary padding in large screens from header CSS.

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,38 @@
 import Header from "../elements/Header.jsx";
+import {useEffect, useState} from "react";
+import {fetchAPI} from "../utils/utils.jsx";
+import DitherImage from "../elements/DitherImage.jsx";
 
 const Categories = ({}) => {
+
+    const [categoryList, setCategoryList] = useState([]);
+
+    useEffect(() => {
+        const getLists = async() => {
+            const cat = await fetchAPI("lists", "en");
+            console.log(cat)
+            setCategoryList(cat.docs[0].items)
+        }
+        getLists();
+    }, []);
+
+
     return (
         <div>
             <Header landing={true}/>
+            <section>
+                {categoryList.map(category => {
+                    console.log(category);
+                    return(
+                        <section className={"container__categories"}>
+                            <DitherImage url={category.item.value.media.hero.sizes.tablet.url}/>
+                            <div>
+                                <h1>{category.item.value.categoryTitle}</h1>
+                            </div>
+                        </section>
+                    )
+                })}
+            </section>
         </div>
     )
 }
