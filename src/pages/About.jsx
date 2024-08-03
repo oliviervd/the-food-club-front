6466cd3a1d1a747aaa076a660f34c99d65c4266b
oliviervd <oliviervandhuynslager@gmail.com>
2024-08-03 16:32:15 +0200

    Fetch dynamic content in About page
    Replaced hardcoded text with data fetched from the API. Updated the styles to accommodate the dynamically loaded content, ensuring proper layout and alignment.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,30 @@
 import Header from "../elements/Header.jsx";
+import {useEffect, useState} from "react";
+import {fetchAPI} from "../utils/utils.jsx";
+import serialize from "../utils/serialize.jsx";
 
 const About = ({}) => {
+
+    const [about, setAbout] = useState([]);
+
+    useEffect(() => {
+        const getAbout = async() => {
+            const about = await fetchAPI("globals", "en");
+            setAbout(about.docs[0].texts)
+        }
+        getAbout();
+    }, []);
+
+    console.log(about[0])
+
     return(
         <div>
             <Header landing={true}/>
-            <div className={"about"}>
-                <h1>RULE #1.</h1>
-                <p>ipsem lorum la autro quisto</p>
-                <h1>RULE #2.</h1>
-                <p>ipsem lorum la autro quisto</p>
-                <h1>RULE #3.</h1>
-                <p>ipsem lorum la autro quisto</p>
-            </div>
+            {about[0] &&
+                <div className={"about"}>
+                    {serialize(about[0].text)}
+                </div>
+            }
         </div>
     )
 }
