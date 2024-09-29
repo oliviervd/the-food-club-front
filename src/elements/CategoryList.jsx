e1a2cd14b0bfc13d35eb7f635d021bff878afd36
oliviervd <oliviervandhuynslager@gmail.com>
2024-09-29 10:18:44 +0200

    Add category subtitle and new font; refine styles
    Included category subtitle rendering in CategoryList.jsx. Added and referenced "DM-serif-display-italic" font. Updated styles for better positioning and appearance of the subtitle text.

--- src/elements/CategoryList.jsx
+++ src/elements/CategoryList.jsx
@@ -14,11 +14,13 @@ const CategoryList = ({data}) => {
             <section className="category-list__container">
                 {categories.map((cat, index)=>{
                     const _cat = cat.item.value;
+                    console.log(_cat)
                     if (_cat.media.hero) {
                         return(
                             <div key={index} className={"category-list__box"} onClick={()=>{navigateTo(_cat.url)}}>
                                 <DitherImage url={_cat.media.hero.sizes.tablet.url}/>
                                 <h2>{_cat.categoryTitle}</h2>
+                                <p>{_cat.categorySubTitle}</p>
                             </div>
                         )
                     }}
