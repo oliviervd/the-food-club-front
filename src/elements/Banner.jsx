f24f0c745f130fb7439a810906f886084f45b315
oliviervd <oliviervandhuynslager@gmail.com>
2024-09-29 11:44:58 +0200

    Improve banner styling and add tilde separators
    In the `fonts.css`, adjusted banner height, added padding, and set white-space to nowrap for better appearance. In `Banner.jsx`, added tilde separators between repeated content for clear differentiation.

--- src/elements/Banner.jsx
+++ src/elements/Banner.jsx
@@ -2,9 +2,15 @@ const Banner = ({content}) => {
     return (
         <div className="scrolling-banner-container">
             <h2 className="banner">{content}</h2>
+            <h2 className="banner">~</h2>
             <h2 className="banner">{content}</h2>
+            <h2 className="banner">~</h2>
             <h2 className="banner">{content}</h2>
+            <h2 className="banner">~</h2>
             <h2 className="banner">{content}</h2>
+            <h2 className="banner">~</h2>
+            <h2 className="banner">{content}</h2>
+            <h2 className="banner">~</h2>
         </div>
     )
 }
