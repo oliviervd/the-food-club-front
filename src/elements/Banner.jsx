c9f301d2f361505a3883d231dcc02909fca89e90
oliviervd <oliviervandhuynslager@gmail.com>
2024-09-29 19:37:34 +0200

    Replace custom scrolling banner with react-fast-marquee
    Removed custom CSS animation and HTML for the scrolling banner, replacing it with the `react-fast-marquee` library to simplify and improve the banner component. Updated package.json and package-lock.json to include the new library.

--- src/elements/Banner.jsx
+++ src/elements/Banner.jsx
@@ -1,27 +1,11 @@
+import Marquee from "react-fast-marquee";
+
 const Banner = ({content}) => {
     return (
-        <div className="scrolling-banner-container">
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-            <h2 className="banner">{content}</h2>
-            <h2 className="banner">~</h2>
-        </div>
+        <Marquee className={"banner"} speed={30} pauseOnHover={false} gradient={false} autoFill={true}>
+                <h2>{content}</h2>
+                <h2>~</h2>
+        </Marquee>
     )
 }
 
