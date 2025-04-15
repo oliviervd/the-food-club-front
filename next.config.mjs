f688894520defbb9a2266c150929a4b025659fef
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-04-15 17:50:58 +0200

    fix ii8n redirect.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 const nextConfig = {
     reactStrictMode: true,
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+        localeDetection: false, // 👈 prevents redirect to /en
+    },
 }
 
 export default nextConfig
