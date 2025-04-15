28d9d6b4551a59bef6fe1f88737507972dd121b7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-04-15 14:22:04 +0200

    update og:description

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,11 @@ import HomeClient from './homeClient.jsx';
 
 export const metadata = {
     title: "Food Club - Home",
-    description: "Food Club is",
+    description: "#1 Don't talk about foodclub - but psssst…. please spread the word! — #2 The foodclub is a curated space focused on quality, featuring only restaurants we've personally visited.",
     keywords: ['food club', 'gent', 'brussel', 'antwerpen'],
     openGraph: {
         title: "Food Club",
-        description: "",
+        description: "#1 Don't talk about foodclub - but psssst…. please spread the word! — #2 The foodclub is a curated space focused on quality, featuring only restaurants we've personally visited.",
         url: "",
         type: "website",
         images: [
