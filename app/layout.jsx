72726d97459d0adb970b08a73bded458da1ed03b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-04-15 18:09:48 +0200

    update metadata for SEO on root

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,31 @@ import 'leaflet/dist/leaflet.css'
 import '/styles/map.css'
 import '/styles/colors.css'
 
+// SEO metadata as defined
+export const metadata = {
+    title: "Food Club - Home",
+    description: "#1 Don't talk about foodclub - but psssst…. please spread the word! — #2 The foodclub is a curated space focused on quality, featuring only restaurants we've personally visited.",
+    keywords: ['food club', 'gent', 'brussel', 'antwerpen'],
+    openGraph: {
+        title: "Food Club",
+        description: "#1 Don't talk about foodclub - but psssst…. please spread the word! — #2 The foodclub is a curated space focused on quality, featuring only restaurants we've personally visited.",
+        url: "https://www.thefoodclub.be",
+        type: "website",
+        images: [
+            {
+                url: "https://www.thefoodclub.be/assets/img/logo-blue.png",
+                alt: "logo of Food Club",
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Food Club',
+        description: '#1 Don’t talk about foodclub...',
+        images: ['https://www.thefoodclub.be/img/logo-blue.png'],
+    },
+};
+
 export default function RootLayout({ children }) {
     const [queryClient] = useState(() => new QueryClient({
         defaultOptions: {
