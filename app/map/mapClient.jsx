d0f9e185d3149ab3ecd7d729a38ecaf077605b75
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-04-19 10:41:26 +0200

    #62 add filters to show venues from a particular day - fix styling

--- app/map/mapClient.jsx
+++ app/map/mapClient.jsx
@@ -4,7 +4,7 @@ import Header from "/components/Header.jsx"
 import {MapContainer, TileLayer, useMap, Marker, Popup} from "react-leaflet";
 import {divIcon, Icon} from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
-import {useContext, useState, useEffect} from "react";
+import {useContext, useState, useEffect, useCallback} from "react";
 import {useRouter} from "next/navigation";
 import {venueStatus, fetchAPI, getCSSVariableValue} from "/utils/utils.jsx";
 import {LocationColorContext} from "/contexts/LocationColorContext.jsx";
