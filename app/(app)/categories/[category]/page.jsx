2508bc963568d0ee73d3f16bdf5c2ab94406fa8b
oliviervd <oliviervandhuynslager@gmail.com>
2025-07-12 11:09:47 +0200

    add meta tags for SEO categories

--- app/(app)/categories/[category]/page.jsx
+++ app/(app)/categories/[category]/page.jsx
@@ -4,9 +4,7 @@ import {getPayload} from "payload";
 export async function generateMetadata({ params }){
   // seo here
 
-  console.log(params.category)
   let query = `https://thefoodclub.be/api/categories?where[url][equals]=${params.category}&depth=0`
-  console.log(query)
   const res = await fetch(query, { cache: 'no-store' });
 
   const data = await res.json();
@@ -22,6 +20,31 @@ export async function generateMetadata({ params }){
   return {
     title: category.meta.title,
     description: category.meta.description,
+    image: category.meta.image.url,
+    keywords: ["review", "food club", category.meta.title], // todo: add these as a separate SEO field in the collection backend
+    openGraph: {
+      title: category.meta.title,
+      description: category.meta.description,
+      images: [{url: category.meta.image.url, alt: `image of ${category.meta.title}`}],
+      url: `https://thefoodclub.be/categories/${category.url}`,
+      type: 'website',
+      site_name: 'The Food Club',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: category.meta.title,
+      description: category.meta.description,
+      images: [{url: category.meta.image.url, alt: `image of ${category.meta.title}`}],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        noImageIndex: false,
+      }
+    },
   }
 }
 
